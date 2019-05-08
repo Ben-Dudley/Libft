ed220a1b6f920cc6df4cd8737174e09761d0cec9
Ben-Dudley <vilenaguslyakova@gmail.com>
2019-05-08 17:35:13 +0300

    libft

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdudley <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/01 17:07:27 by bdudley           #+#    #+#             */
+/*   Updated: 2019/03/22 12:10:39 by bdudley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *p;
+
+	p = (unsigned char *)s;
+	while (n-- > 0)
+	{
+		if (*p == (unsigned char)c)
+			return ((void *)p);
+		p++;
+	}
+	return (NULL);
+}
